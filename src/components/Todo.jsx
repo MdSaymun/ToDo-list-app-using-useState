798f2794bd8f5c6d59232ea26eea7a7665b25347
MdSaymun <saymun2000@gmail.com>
2023-07-21 10:38:23 +0600

    to do app initilized

---
+++ src/components/Todo.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import { BsFillTrashFill } from "react-icons/bs";
+import { AiOutlineCheckCircle, AiFillCheckCircle } from "react-icons/ai";
+
+const Todo = ({ propsObj }) => {
+  const { todos, completeTodo, removeTodo } = propsObj;
+
+  return todos.map((todo) => (
+    <div className={todo.isComplete ? "todo-row complete" : "todo-row"} key={todo.id}>
+      <div>{todo.text}</div>
+      <div className="icons">
+        <BsFillTrashFill onClick={() => removeTodo(todo.id)} className="delete-icon" />
+        {todo.isComplete ? (
+          <AiFillCheckCircle onClick={() => completeTodo(todo.id)} className="delete-icon" />
+        ) : (
+          <AiOutlineCheckCircle onClick={() => completeTodo(todo.id)} className="delete-icon" />
+        )}
+      </div>
+    </div>
+  ));
+};
+
+export default Todo;
+
+// propsType
+Todo.propTypes = {
+  propsObj: {
+    todos: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+        isComplete: PropTypes.bool.isRequired,
+      })
+    ).isRequired,
+    completeTodo: PropTypes.func.isRequired,
+    removeTodo: PropTypes.func.isRequired,
+  },
+};
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>To-Do List</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo propsObj={{ todos, completeTodo, removeTodo }} />
+    </>
+  );
+}
+
+export default TodoList;
