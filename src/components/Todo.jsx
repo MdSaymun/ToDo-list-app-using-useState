543b16a76581c3e0449ed487d63bee5ab3163cd6
MdSaymun <saymun2000@gmail.com>
2023-07-21 11:26:51 +0600

    some modification

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,14 +7,16 @@ const Todo = ({ propsObj }) => {
 
   return todos.map((todo) => (
     <div className={todo.isComplete ? "todo-row complete" : "todo-row"} key={todo.id}>
-      <div>{todo.text}</div>
+      <div className="icons">
+        {todo.isComplete ? (
+          <AiFillCheckCircle onClick={() => completeTodo(todo.id)} className="completed-icon" />
+        ) : (
+          <AiOutlineCheckCircle onClick={() => completeTodo(todo.id)} className="complete-icon" />
+        )}
+      </div>
+      <div className="text">{todo.text}</div>
       <div className="icons">
         <BsFillTrashFill onClick={() => removeTodo(todo.id)} className="delete-icon" />
-        {todo.isComplete ? (
-          <AiFillCheckCircle onClick={() => completeTodo(todo.id)} className="delete-icon" />
-        ) : (
-          <AiOutlineCheckCircle onClick={() => completeTodo(todo.id)} className="delete-icon" />
-        )}
       </div>
     </div>
   ));
